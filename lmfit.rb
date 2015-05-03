5f9e2a11d627f6e1061dfa43926db2d6e8a3194c
Nils Berg <berg.nils@gmail.com>
2015-05-03 14:31:34 +0200

    copy demo source to share folder
    instead of having the test code in the formula, it's now installed to share/lmfit/demos

--- lmfit.rb
+++ lmfit.rb
@@ -7,97 +7,19 @@ class Lmfit < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+    # Copy the demo source to share folder
+    (share/"lmfit/demos").install Dir["demo/*.c"]
   end
 
+
   test do
-    (testpath/"test.c").write <<-EOS
-      #include "lmcurve.h"
-      #include "lmmin.h"
+    # Compile the demo programs from share and check that they exit nonzero
 
-      /* model function: a parabola */
-
-      double f_curve( double t, const double *p )
-      {
-        return p[0] + p[1]*t + p[2]*t*t;
-      }
-
-      /* fit model: a plane p0 + p1*tx + p2*tz */
-      double f_min( double tx, double tz, const double *p )
-      {
-          return p[0] + p[1]*tx + p[2]*tz;
-      }
-
-      /* data structure to transmit arrays and fit model */
-      typedef struct {
-          double *tx, *tz;
-          double *y;
-          double (*f)( double tx, double tz, const double *p );
-      } data_struct;
-
-      /* function evaluation, determination of residues */
-      void evaluate_surface( const double *par, int m_dat, const void *data,
-                             double *fvec, int *info )
-      {
-          /* for readability, explicit type conversion */
-          data_struct *D;
-          D = (data_struct*)data;
-
-          int i;
-          for ( i = 0; i < m_dat; i++ )
-               fvec[i] = D->y[i] - D->f( D->tx[i], D->tz[i], par );
-      }
-
-      int main()
-      {
-        // Evaluate lmcurve
-        {
-          int n = 3; /* number of parameters in model function f */
-          double par[3] = { 100, 0, -10 }; /* really bad starting value */
-
-          /* data points: a slightly distorted standard parabola */
-          int m = 9;
-          int i;
-          double t[9] = { -4., -3., -2., -1.,  0., 1.,  2.,  3.,  4. };
-          double y[9] = { 16.6, 9.9, 4.4, 1.1, 0., 1.1, 4.2, 9.3, 16.4 };
-
-          lm_control_struct control = lm_control_double;
-          lm_status_struct status;
-          control.verbosity = 9;
-
-          /* now the call to lmfit */
-          lmcurve( n, par, m, t, y, f_curve, &control, &status );
-        }
-
-        {
-          //Evaluate lmmin
-
-          /* parameter vector */
-          int n_par = 3;                /* number of parameters in model function f */
-          double par[3] = { -1, 0, 1 }; /* arbitrary starting value */
-
-          /* data points */
-          int m_dat = 4;
-          double tx[4] = { -1, -1,  1,  1 };
-          double tz[4] = { -1,  1, -1,  1 };
-          double y[4]  = {  0,  1,  1,  2 };
-
-          data_struct data = { tx, tz, y, f_min };
-
-          /* auxiliary parameters */
-          lm_status_struct status;
-          lm_control_struct control = lm_control_double;
-          control.verbosity = 9;
-
-          /* perform the fit */
-          lmmin( n_par, par, m_dat, (const void*) &data,
-                 evaluate_surface, &control, &status );
-        }
-
-        return 0;
-      }
-    EOS
-
-    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-llmfit", "-o", "test"
-    system "./test"
+    # curve1.c tests lmcurve.h
+    system ENV.cc, (share/"lmfit/demos/curve1.c"), "-I#{include}", "-L#{lib}", "-llmfit", "-o", "curve1"
+    system "./curve1"
+    # surface1.c tests lmmin.h
+    system ENV.cc, (share/"lmfit/demos/surface1.c"), "-I#{include}", "-L#{lib}", "-llmfit", "-o", "surface1"
+    system "./surface1"
   end
 end
